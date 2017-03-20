14eb7ed337de089f6a867a7e9cead2d1554d3e9e
Neill Giraldo <neillro24124@gmail.com>
2017-03-20 12:40:49 -0500

    Improved layer logical representation between service and model

--- delete_ms/model/File.py
+++ delete_ms/model/File.py
@@ -1,36 +1,24 @@
 from couchdb.mapping import Document, TextField, IntegerField, \
     DateTimeField, ListField, DecimalField
 from datetime import datetime, timedelta, time
+from couchdb import Server
 
+
+host = "192.168.99.101"
+port = "3010"
+url = "http://{0}:{1}/".format(host,port)
+server = Server(url=url)
+db = server["file"]
 
 class File(Document):
 
-    def __init__(obj):
-        self._id = obj['_id']
-        self._rev = obj['_rev']
-        self.name = obj['name']
-        self.extension = obj['extension']
-        self.size = obj['size']
-        self.uploaded_date = obj['uploaded_date']
-        self.expiring_date = obj['expiring_date']
-        self.owner_id = obj['owner_id']
-        self.shared = obj['shared']
+    @staticmethod
+    def get_doct(id):
+        return db[str(id)]
 
-    _id = TextField()
-    _rev = TextField()
-    name = TextField()
-    extension = TextField()
-    size = IntegerField()
-
-    # If the upload_date isn't given, upload_date will be the today date in seconds since the Epoch
-
-    uploaded_date = DecimalField(default=time)
-
-    # If the expiring_date isn't given, expiring_date will be today date in a week in seconds since the Epoch
-
-    expiring_date = DecimalField()
-    owner_id = TextField()
-    shared = ListField(TextField())
+    @staticmethod
+    def delete_doct(deleted_file):
+        db.delete(deleted_file)
 
 
 
--- delete_ms/service/Delete_Service.py
+++ delete_ms/service/Delete_Service.py
@@ -1,33 +1,25 @@
 from model.File import File
-from couchdb import Server
 import requests
 import json
 
-host = "192.168.99.101"
-port = "3010"
-url = "http://{0}:{1}/".format(host,port)
-server = Server(url=url)
+
 
 class Delete_Service:
 
 
     def __init__(self,id):
-    	#consult_data_base
-        db = server["file"]
+    	#consult_data_base 
         try:
-            obj= db[str(id)]
-            self.document = obj
+            self.document = File.get_doct(id)
             self.warning = ""
-        except:
+        except Exception as e:
             self.document = None
             self.warning = "Unexisting Document"
 
     def delete(self):
         #delete
-        db = server["file"]
         if self.document != None:
-            deleted_file = self.document
-            db.delete(deleted_file)
+            File.delete_doct(self.document)
             return "",204
         else:
             return self.warning,404
