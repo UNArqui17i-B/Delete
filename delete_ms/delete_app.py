d0e1932e455e6aa642be090bcaa0ec5fb265c9d0
Neill Giraldo <neillro24124@gmail.com>
2017-03-21 16:19:12 -0500

    Added env variables

--- delete_ms/delete_app.py
+++ delete_ms/delete_app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from resource.Delete_Resource import Delete_Resource
+from os import env
 
 app = Flask(__name__,static_folder="static");
 api = Api(app)
@@ -14,4 +15,4 @@ def hello_docker():
 if __name__ == "__main__":
     app.debug = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(host="0.0.0.0",port=4030)
+    app.run(host=env["BIND_ADDRESS"],port=env["HOST_PORT"])
